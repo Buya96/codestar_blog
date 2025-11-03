e511407ccc44a03d3526735e07ae27a5002dcce9
Buya96 <ayub1231@hotmail.com>
2025-11-03 16:21:55 +0000

    secret key for production use.

--- codestar/env.py
+++ codestar/env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "<postgresql://neondb_owner:npg_8hovRyXafJW4@ep-shy-dawn-agpn7wlc.c-2.eu-central-1.aws.neon.tech/stung_baggy_cushy_537288>")
+os.environ.setdefault("SECRET_KEY", "<WaSbC1GSzy>")
